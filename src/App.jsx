74c29a27df000e46bb72f817f2f3d2557ec06be1
-_- <majest228@gmail.com>
2024-07-07 21:38:15 +0500

    Добавление telegram api

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 
 function App() {
 	const [count, setCount] = useState(0)
+
+	useEffect(() => {
+		if (window.Telegram.WebApp) {
+			window.Telegram.WebApp.expand()
+		}
+	}, [])
 
 	return (
 		<>
