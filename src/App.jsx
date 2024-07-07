3b69e536cbceacc21eb38d52d68531b5d720b338
-_- <majest228@gmail.com>
2024-07-08 00:33:04 +0500

    Обнова в зайце

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 import monkey from './assets/1.png'
-
+import money from './assets/money.png'
 function App() {
 	const [count, setCount] = useState(0)
 	const [touchStarted, setTouchStarted] = useState(false)
-
+	const levelUp = 10000
 	useEffect(() => {
 		if (window.Telegram.WebApp) {
 			window.Telegram.WebApp.expand()
 		}
+	}, [])
+
+	useEffect(() => {
+		const intervalId = setInterval(() => {
+			setCount(state => state + 1)
+		}, 1000)
+
+		return () => clearInterval(intervalId)
 	}, [])
 
 	const handleTouchMove = () => {
@@ -22,20 +30,31 @@ function App() {
 	const handleTouchEnd = () => {
 		setTouchStarted(false)
 	}
-
+	const progress = Math.min((count / levelUp) * 100, 100)
 	return (
 		<div className='app'>
-			<h3>{count}</h3>
-			<button
-				className='app-button'
-				onTouchMove={handleTouchMove}
-				onTouchEnd={handleTouchEnd}
-				onClick={() => {
-					setCount(state => state + 1)
-				}}
-			>
-				<img src={monkey} alt='monkey' />
-			</button>
+			<div className='app-container'>
+				<div className='app-content'>
+					<div className='app-money'>
+						<img src={money} alt='money' />
+						<span className='app-money__text'>{count}</span>
+					</div>
+					<div
+						className='app-bar'
+						style={{ width: `${progress}%`, background: '#fff' }}
+					></div>
+					<button
+						className='app-button'
+						onTouchMove={handleTouchMove}
+						onTouchEnd={handleTouchEnd}
+						onClick={() => {
+							setCount(state => state + 1)
+						}}
+					>
+						<img src={monkey} alt='monkey' />
+					</button>
+				</div>
+			</div>
 		</div>
 	)
 }
